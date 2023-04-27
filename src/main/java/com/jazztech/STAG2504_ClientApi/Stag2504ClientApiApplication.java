e718d68305755d03f4459f399ba25a724483c4b8
BrunOshiro <86211029+BrunOshiro@users.noreply.github.com>
2023-04-27 14:38:45 -0300

    ajuste para comunicar com api do correio

--- src/main/java/com/jazztech/STAG2504_ClientApi/Stag2504ClientApiApplication.java
+++ src/main/java/com/jazztech/STAG2504_ClientApi/Stag2504ClientApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Stag2504ClientApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Stag2504ClientApiApplication.class, args);
 	}
--- src/main/java/com/jazztech/STAG2504_ClientApi/infrastructure/apiClients/ViaCepApiClient.java
+++ src/main/java/com/jazztech/STAG2504_ClientApi/infrastructure/apiClients/ViaCepApiClient.java
@@ -1,26 +1,12 @@
 package com.jazztech.STAG2504_ClientApi.infrastructure.apiClients;
 
 import com.jazztech.STAG2504_ClientApi.infrastructure.apiClients.dto.AddressDto;
-import com.jazztech.STAG2504_ClientApi.infrastructure.exceptions.ApiClientException;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-public class ViaCepApiClient {
-
-    private final RestTemplate restTemplate;
-
-    public ViaCepApiClient(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
-    }
-
-    public AddressDto getAddressByCep(String cep) throws ApiClientException {
-        String url = "https://viacep.com.br/ws/" + cep + "/json/";
-        ResponseEntity<AddressDto> response = restTemplate.getForEntity(url, AddressDto.class);
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return response.getBody();
-        } else {
-            throw new ApiClientException("Não foi possível encontrar informações sobre o endereço");
-        }
-    }
+@FeignClient(url = "https://viacep.com.br/ws", name = "ViaCepApi")
+public interface ViaCepApiClient {
+    @GetMapping("/{cep}/json")
+    AddressDto address(@PathVariable String cep);
 }
