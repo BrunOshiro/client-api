3ae66a1732fac49a67524687173090b384a41278
BrunOshiro <86211029+BrunOshiro@users.noreply.github.com>
2023-04-28 18:05:35 -0300

    retirando códigos não utilizados configuração FeignClients

--- src/main/java/com/jazztech/STAG2504_ClientApi/Stag2504ClientApiApplication.java
+++ src/main/java/com/jazztech/STAG2504_ClientApi/Stag2504ClientApiApplication.java
@@ -1,13 +1,19 @@
 package com.jazztech.STAG2504_ClientApi;
 
-import org.springframework.boot.SpringApplication;
+import com.jazztech.STAG2504_ClientApi.applicationService.clientsService.CreateClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.SpringApplication;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @EnableFeignClients
 @SpringBootApplication
 public class Stag2504ClientApiApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CreateClient.class);
 	public static void main(String[] args) {
+		LOGGER.info("Inicializando aplicação");
 		SpringApplication.run(Stag2504ClientApiApplication.class, args);
+		LOGGER.info("Aplicação inicializada");
 	}
 }
