ae8b5e2f9442f732806117bdf1e27e428b019412
BrunOshiro <86211029+BrunOshiro@users.noreply.github.com>
2023-05-08 09:17:20 -0300

    refactor - Ajustado o services e controller para nova estrutura

--- src/main/java/com/jazztech/STAG2504_ClientApi/infrastructure/repository/ClientsRepository.java
+++ src/main/java/com/jazztech/STAG2504_ClientApi/infrastructure/repository/ClientsRepository.java
@@ -2,10 +2,9 @@ package com.jazztech.STAG2504_ClientApi.infrastructure.repository;
 
 import com.jazztech.STAG2504_ClientApi.infrastructure.repository.entity.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import java.util.UUID;
 
-public interface ClientsRepository extends JpaRepository <ClientEntity, Long>{
+public interface ClientsRepository extends JpaRepository <ClientEntity, UUID>{
     List<ClientEntity> findByCpf(String cpf);
-
 }
--- src/main/java/com/jazztech/STAG2504_ClientApi/presentation/dto/ClientDto.java
+++ src/main/java/com/jazztech/STAG2504_ClientApi/presentation/dto/ClientDto.java
@@ -1,9 +1,11 @@
 package com.jazztech.STAG2504_ClientApi.presentation.dto;
 
 import java.time.LocalDate;
+import java.util.UUID;
+
 //Dados que serão enviados/recebidos para/pelo usuário
 public record ClientDto (
-        Long id,
+        UUID id,
         String nome,
         String cpf,
         LocalDate dataNascimento,
